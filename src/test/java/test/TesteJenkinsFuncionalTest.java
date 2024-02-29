f4c733e8a74b3e0e64be06a46314101dc453cd14
Pedro Bersch <pedroleottebersch@gmail.com>
2024-02-29 17:06:26 -0300

    docs: creating branch for jenkins configurate

---
+++ src/test/java/test/TesteJenkinsFuncionalTest.java
@@ -0,0 +1,11 @@
+package test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TesteJenkinsFuncionalTest {
+    @Test
+    public void testExemplo(){
+        Assert.assertEquals(2,1+1);
+    }
+}
